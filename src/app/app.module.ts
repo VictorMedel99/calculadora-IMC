c79a457d12eda3d017bb0048939698fb03548a61
VictorMedel99 <victor.medelito@gmail.com>
2020-12-04 13:33:43 -0600

    add IMCUI code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
+import { IMCUIComponent } from './imcui/imcui.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
+    IMCUIComponent,
   ],
   imports: [
     BrowserModule,
